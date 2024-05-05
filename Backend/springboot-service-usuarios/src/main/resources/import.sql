31365fd2871f867ce7bce4b93dfcbd7352f884db
Hugo Ovalle <64418752+HugoOvalle73@users.noreply.github.com>
2024-05-05 00:53:52 -0600

    Tarea 6 Final
    Avance de todos los videos

--- Backend/springboot-service-usuarios/src/main/resources/import.sql
+++ Backend/springboot-service-usuarios/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('gustavoo','12345',1,'Gustavo','Rodriguez','gustavo@uanl.com')
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('hugoo','12345',1,'Hugo','Ovalle','hugo@uanl.com')
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('gustavoo','$2a$10$9IUMs8oyaVWzQYK6uxXn2.9Ujv0SxGGvN5c8T4yQ7zOUe9Ow41Z.W',1,'Gustavo','Rodriguez','gustavo@uanl.com')
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('hugoo','$2a$10$OtPH7bDkfoJKf1zfN3W1EOde8D6p0gIu6Ym1es7ZoDKBrwgVqjPSO',1,'Hugo','Ovalle','hugo@uanl.com')
 
 INSERT INTO roles (nombre) VALUES ('ROLE_USER');
 INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
